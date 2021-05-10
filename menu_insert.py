1c914ed3f136f0f63e0ae1ae151a0d182f202c27
kangyeson <s2019w01@e-mirim.hs.kr>
2021-05-11 05:34:19 +0900

    Dutch Project 최종 완성

---
+++ menu_insert.py
@@ -0,0 +1,226 @@
+import tkinter
+import tkinter.ttk
+import tkinter.font
+import tkinter.messagebox
+from tkinter import *
+import datetime
+import random
+
+import pymysql
+
+import receipt
+import numPeople
+
+class Menuinsert:
+
+    def __init__(self, menu, n):
+        self.menu = menu
+        self.personnum = n
+        self.personname = [0, 0, 0, 0, 0, 0]
+        self.personmenu = [0, 0, 0, 0, 0, 0]
+        self.persontotal =[0, 0, 0, 0, 0, 0]
+
+        # 배경 설정
+        self.menuBack = tkinter.PhotoImage(file="image/white.png")
+        self.menuBackL = tkinter.Label(image=self.menuBack)
+        self.menuBackL.place(x=-2, y=-2)
+        # 폰트 설정
+        fonts = tkinter.font.Font(size=11, weight='bold')
+        fontm = tkinter.font.Font(size=13, weight='bold')
+        # 장소 입력
+        self.locationText = Label(menu, text='장소', width=6, bg="white", font=fontm, fg='#ff7878')
+        self.locationText.place(x=100, y=70)
+
+        # 입력수에 제한받지 않고 필수로 나타나는 칸
+
+        # 위치 입력칸
+        self.inputLocation = Entry(self.menu, width=23, font=fonts, relief="groove", highlightthickness=2,
+                                   highlightbackground="#8294cd")
+        self.inputLocation.place(x=100, y=100)
+        # 뒤로 가기 버튼
+        self.backButton = Button(self.menu, width=4, text='⇦', repeatdelay=20, bg='#ff7878', font=fontm,
+                                 fg="white", command=self.moveTonumPeople)  # command=self.to_receipt,
+        self.backButton.place(x=20, y=25)
+        # 도움말 버튼
+        self.infoButton = Button(self.menu, width=5, text='도움말', repeatdelay=20, bg='#ff7878', font=fontm,
+                                 fg="white", command=self.Msgbox)  # command=self.to_receipt,
+        self.infoButton.place(x=800, y=25)
+        # 메뉴 입력
+        self.menuText = Label(self.menu, text='더치페이 할 품목', width=20, bg="white", font=fontm, fg='#ff7878')
+        self.menuText.place(x=65, y=140)
+
+        self.inputMenu = Entry(self.menu, width=80, font=fonts, relief="groove", highlightthickness=2,
+                               highlightbackground="#8294cd")
+        self.inputMenu.place(x=100, y=170)
+
+        # 이름 입력
+        self.locationText = Label(self.menu, text='이름', width=10, bg="white", font=fontm, fg='#ff7878')
+        self.locationText.place(x=70, y=210)  # 410
+        # 사람1
+        self.personname[1 - 1] = Entry(self.menu, width=20, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 사람2
+        self.personname[2 - 1] = Entry(self.menu, width=20, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 사람3
+        self.personname[3 - 1] = Entry(self.menu, width=20, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 사람4
+        self.personname[4 - 1] = Entry(self.menu, width=20, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 사람5
+        self.personname[5 - 1] = Entry(self.menu, width=20, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 사람6
+        self.personname[6 - 1] = Entry(self.menu, width=20, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+
+        # 사람 수 만큼 칸 보여주기
+        for i in range(0, self.personnum):
+            self.personname[i].place(x=100, y=220 + 30 * (i + 1))
+
+        # 메뉴 가격
+        self.priceText = Label(self.menu, text='개인 메뉴 가격', width=10, bg="white", font=fontm, fg='#ff7878')
+        self.priceText.place(x=330, y=210)  # 410
+        # 메뉴1
+        self.personmenu[1 - 1] = Entry(self.menu, width=50, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 메뉴2
+        self.personmenu[2 - 1] = Entry(self.menu, width=50, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 메뉴3
+        self.personmenu[3 - 1] = Entry(self.menu, width=50, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 메뉴4
+        self.personmenu[4 - 1] = Entry(self.menu, width=50, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 메뉴5
+        self.personmenu[5 - 1] = Entry(self.menu, width=50, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 메뉴6
+        self.personmenu[6 - 1] = Entry(self.menu, width=50, font=fonts, relief="groove", bg='white', fg='black',
+                                       highlightthickness=1, highlightbackground="#8294cd")
+        # 단체메뉴
+        self.groupmenuText = Label(self.menu, text='단체메뉴 가격', width=10, bg="white", font=fontm, fg='#ff7878')
+        self.groupmenuText.place(x=100, y=450)
+        # 단체메뉴
+        self.groupmenu = Entry(self.menu, width=55, font=fonts, relief="groove", bg='white', fg='black',
+                               highlightthickness=1, highlightbackground="#8294cd")
+        self.groupmenu.place(x=100, y=500)
+
+        # 사람 수 만큼 메뉴칸 보여주기
+        for i in range(0, self.personnum):
+            self.personmenu[i].place(x=330, y=220 + 30 * (i + 1))
+
+        # 반올림
+        self.updown = Label(self.menu, text='반올림 단위 ', width=10, bg="white", font=fontm, fg='#ff7878')
+        self.updown.place(x=100, y=550)
+
+        # 라디오 버튼
+        self.RadioVariety_1 = tkinter.IntVar()
+
+        self.rd1 = Radiobutton(self.menu, text="10", bg="white", font=fontm, fg='#ff7878', value=1,
+                               variable=self.RadioVariety_1)
+        self.rd1.place(x=100, y=600)
+
+        self.rd2 = Radiobutton(self.menu, text="100", bg="white", font=fontm, fg='#ff7878', value=2,
+                               variable=self.RadioVariety_1)
+        self.rd2.place(x=200, y=600)
+
+        self.rd3 = Radiobutton(self.menu, text="1000", bg="white", font=fontm, fg='#ff7878', value=3,
+                               variable=self.RadioVariety_1)
+        self.rd3.place(x=300, y=600)
+
+        self.rd4 = Radiobutton(self.menu, text="10000", bg="white", font=fontm, fg='#ff7878', value=4,
+                               variable=self.RadioVariety_1)
+        self.rd4.place(x=400, y=600)
+
+        self.rd5 = Radiobutton(self.menu, text="반올림 안 함", bg="white", font=fontm, fg='#ff7878', value=5,
+                               variable=self.RadioVariety_1)
+        self.rd5.place(x=500, y=600)
+
+        # 저장 버튼
+        self.saveButton = Button(self.menu, width=30, text='G O !', repeatdelay=20, bg='#ff7878',
+                                 font=fontm, fg="white", command=self.save)  # command=self.to_receipt,
+        self.saveButton.place(x=300, y=650)
+
+
+    def moveTonumPeople(self):
+        Move = numPeople.NumPeople(self.menu)
+
+    def play(self):
+        self.menu.mainloop()
+
+    def Msgbox(self):
+        tkinter.messagebox.showinfo("도움말", "1.이름 밑 칸에는 이름을 입력해주세요."
+                                           "\n2.가격을 입력하실 때에는 첫번째칸부터 '/'로 가격을 나누어서 꼭 모두 입력해주세요. "
+                                           "\n3.단체메뉴 가격도 '/'로 가격을 나누어서 입력해주세요."
+                                           "\n4.입력을 끝냈다면 GO! 버튼을 눌러주세요.")
+
+    def save(self):
+        for i in range(0, self.personnum):
+            if not self.personname[0].get():
+                tkinter.messagebox.showinfo("이름 입력", "모든 사람의 이름을 입력해주세요.")
+                break
+            if not self.personmenu[i].get():
+                tkinter.messagebox.showinfo("개인메뉴 가격 입력", "모든 개인메뉴 가격을 입력해주세요."
+                                                          "\n개임메뉴가격이 없는 경우 0을 입력해주세요")
+                break
+            if not self.groupmenu.get():
+                tkinter.messagebox.showinfo("그룹메뉴 가격 입력", "그룹메뉴 가격을 입력해주세요."
+                                                          "\n그룹메뉴가격이 없는 경우 0을 입력해주세요")
+                break
+            else:
+                self.ID = random.randint(1, 1000)
+                self.now = datetime.datetime.utcnow()
+                sqlCon = pymysql.connect(host="127.0.0.1", user="root", password="goodday0722", database="dutchdb",
+                                         charset="utf8")
+                cur = sqlCon.cursor(pymysql.cursors.DictCursor)
+                cur.execute(
+                    "INSERT INTO dutchdb VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (
+                        self.ID,
+                        self.now.strftime('%Y-%m-%d %H:%M:%S'),
+                        self.personnum,  # 명수
+                        self.personname[1 - 1].get(),  # 이름1
+                        self.personname[2 - 1].get(),  # 이름2
+                        self.personname[3 - 1].get(),  # 이름3
+                        self.personname[4 - 1].get(),  # 이름4
+                        self.personname[5 - 1].get(),  # 이름5
+                        self.personname[6 - 1].get(),  # 이름6
+
+                        self.personmenu[1 - 1].get(),  # 메뉴1
+                        self.personmenu[2 - 1].get(),  # 메뉴2
+                        self.personmenu[3 - 1].get(),  # 메뉴3
+                        self.personmenu[4 - 1].get(),  # 메뉴4
+                        self.personmenu[5 - 1].get(),  # 메뉴5
+                        self.personmenu[6 - 1].get(),  # 메뉴6
+
+                        self.groupmenu.get(),
+                        self.inputLocation.get(),  # 장소
+                        self.inputMenu.get(),  # 그룹메뉴가격
+                        self.RadioVariety_1.get()
+                    ))
+                sqlCon.commit()
+                sqlCon.close()
+
+                inputm = [self.inputLocation.get(), self.inputMenu.get(),
+                          [self.personname[1 - 1].get(), self.personmenu[1 - 1].get()],
+                          [self.personname[2 - 1].get(), self.personmenu[2 - 1].get()],
+                          [self.personname[3 - 1].get(), self.personmenu[3 - 1].get()],
+                          [self.personname[4 - 1].get(), self.personmenu[4 - 1].get()],
+                          [self.personname[5 - 1].get(), self.personmenu[5 - 1].get()],
+                          [self.personname[6 - 1].get(), self.personmenu[6 - 1].get()],
+                          self.groupmenu.get(), self.RadioVariety_1.get()]
+
+                Move = receipt.Receipt(self.menu, inputm, self.personnum, self.ID)
+                break
+
+if __name__ == '__main__':
+    menu = tkinter.Tk()
+    menu.title("더치 페이")
+    menu.geometry("900x700+100+100")
+    menu.resizable(False, False)
+
+    re = Menuinsert(menu)
+    re.play()
---
+++ receipt.py
@@ -0,0 +1,211 @@
+import tkinter
+import tkinter.ttk
+import datetime
+import tkinter.font
+import pymysql
+import tkinter.messagebox
+
+import menu_insert
+import start
+from tkinter import *
+
+# 개인 메뉴 잘 보여줌
+# 반올림 기능 해야함
+
+class Receipt:
+
+    def __init__(self, receipt, inputmenu, personnum, ID):
+        self.rpertotal = 0
+        self.receipt = receipt
+        self.recentId = ID
+        self.rpersonnum = personnum
+        self.rpersonname = [0, 0, 0, 0, 0, 0]  # 사람 이름
+        self.rpersonmenu = [0, 0, 0, 0, 0, 0]  # 메뉴
+        self.per_sum = [0, 0, 0, 0, 0, 0]  # 개인메뉴 합계
+        self.per_tolsum = [0, 0, 0, 0, 0, 0]  # 계산
+        self.rper_tolsum = [0, 0, 0, 0, 0, 0]  # 텍스트
+        self.round_price = 0  # 반올림 한 총합
+        self.ori_price = 0  # 반올림 전 총합
+
+        # menu_insert에서 배열 받아옴
+        self.person = inputmenu
+        # 받아온 값 새로운 변수에 저장
+        self.rinputLocation = inputmenu[0]
+        self.rinputMenu = inputmenu[1]
+        # 이름
+
+        # 그룹메뉴
+        self.mgroupmenu = inputmenu[8]
+        self.mupdown = self.person[9]  # 라디오 버튼 1 : 10 ,2:100,3 :1000 4:10000 5 : 반올림 안 함
+        # 폰트
+        fonts = tkinter.font.Font(size=10, weight='bold')
+        fontm = tkinter.font.Font(size=14, weight='bold')
+        # 배경
+        self.receiptBack = tkinter.PhotoImage(file="image/receipt_back.png")
+        self.receiptBackL = tkinter.Label(image=self.receiptBack)
+        self.receiptBackL.place(x=-2, y=-2)
+
+        # 뒤로가기 버튼
+        self.backButton = Button(self.receipt, width=4, text='⇦', repeatdelay=20, bg='#ff7878', font=fontm,
+                                 fg="white", command=self.back)  # command=self.to_receipt,
+        self.backButton.place(x=20, y=25)
+        # 저장 버튼
+        self.saveButton = Button(self.receipt, width=30, text='저 장', repeatdelay=20, bg='#ff7878', font=fontm,
+                                 fg="white", command=self.moveToStart)
+        self.saveButton.place(x=300, y=650)
+
+        # 화면 배치하기
+
+        # 위치
+        self.rinputLocation = Label(self.receipt, text=self.rinputLocation, fg='#db4455', font=fontm, bg='white')
+        self.rinputLocation.place(x=130, y=530)
+        # 메뉴
+        self.rinputMenu = Label(self.receipt, text=self.rinputMenu, fg='#db4455', font=fontm, bg='white')
+        self.rinputMenu.place(x=90, y=570)
+        # 사람1이름
+        self.rpersonname[1 - 1] = Label(self.receipt, text=self.person[1 + 1][0], fg='#db4455', font=fontm, bg='white')
+        # 사람2이름
+        self.rpersonname[2 - 1] = Label(self.receipt, text=self.person[2 + 1][0], fg='#db4455', font=fontm, bg='white')
+        # 사람3이름
+        self.rpersonname[3 - 1] = Label(self.receipt, text=self.person[3 + 1][0], fg='#db4455', font=fontm, bg='white')
+        # 사람4이름
+        self.rpersonname[4 - 1] = Label(self.receipt, text=self.person[4 + 1][0], fg='#db4455', font=fontm, bg='white')
+        # 사람5이름
+        self.rpersonname[5 - 1] = Label(self.receipt, text=self.person[5 + 1][0], fg='#db4455', font=fontm, bg='white')
+        # 사람6이름
+        self.rpersonname[6 - 1] = Label(self.receipt, text=self.person[6 + 1][0], fg='#db4455', font=fontm, bg='white')
+
+        # 사람 이름 위치 지정
+        for i in range(0, self.rpersonnum):
+            self.rpersonname[i].place(x=50, y=160 + 40 * (i + 1))
+
+        # 1. /로 나누기 2. 문자로 된 리스트 값 정수로 바꾸기 3. 합계 구하기
+        # person[1+1][0]
+
+        for i in range(0, self.rpersonnum):  # range 명수까지 입력
+            p_split = self.person[i + 2][1].split('/')  # /로 나눠서 split
+            p_trans = map(int, p_split)  # int 타입으로 변환
+            p_sum = sum(p_trans)  # 리스트에 들어있는 값의 합을 구함
+            if self.mupdown == 1:
+                self.per_sum[i] = round(p_sum, -1)
+            elif self.mupdown == 2:
+                self.per_sum[i] = round(p_sum, -2)
+            elif self.mupdown == 3:
+                self.per_sum[i] = round(p_sum, -3)
+            elif self.mupdown == 4:
+                self.per_sum[i] = round(p_sum, -4)
+            else:
+                self.per_sum[i] = p_sum
+
+        # 그룹메뉴
+        g_split = self.mgroupmenu.split('/')
+        self.g_trans = sum(map(int, g_split))
+        self.rgroupmenu = int(self.g_trans / self.rpersonnum)  # 1/n 으로 나눔
+
+        # 그룹메뉴 반올림
+        if self.mupdown == 1:
+            self.rgroupmenup = round(self.rgroupmenu, -1)
+        elif self.mupdown == 2:
+            self.rgroupmenup = round(self.rgroupmenu, -2)
+        elif self.mupdown == 3:
+            self.rgroupmenup = round(self.rgroupmenu, -3)
+        elif self.mupdown == 4:
+            self.rgroupmenup = round(self.rgroupmenu, -4)
+        else:
+            self.rgroupmenup = self.rgroupmenu
+
+        # 사람1메뉴
+        self.rpersonmenu[1 - 1] = Label(self.receipt,
+                                        text=str(self.per_sum[0]) + '\t\t            ' + str(self.rgroupmenup),
+                                        fg='#db4455', font=fontm, bg='white')
+        # 사람2메뉴
+        self.rpersonmenu[2 - 1] = Label(self.receipt,
+                                        text=str(self.per_sum[1]) + '\t\t            ' + str(self.rgroupmenup),
+                                        fg='#db4455', font=fontm, bg='white')
+        # 사람3메뉴
+        self.rpersonmenu[3 - 1] = Label(self.receipt,
+                                        text=str(self.per_sum[2]) + '\t\t            ' + str(self.rgroupmenup),
+                                        fg='#db4455', font=fontm, bg='white')
+        # 사람4메뉴
+        self.rpersonmenu[4 - 1] = Label(self.receipt,
+                                        text=str(self.per_sum[3]) + '\t\t            ' + str(self.rgroupmenup),
+                                        fg='#db4455', font=fontm, bg='white')
+        # 사람5메뉴
+        self.rpersonmenu[5 - 1] = Label(self.receipt,
+                                        text=str(self.per_sum[4]) + '\t\t            ' + str(self.rgroupmenup),
+                                        fg='#db4455', font=fontm, bg='white')
+        # 사람6메뉴
+        self.rpersonmenu[6 - 1] = Label(self.receipt,
+                                        text=str(self.per_sum[5]) + '\t\t            ' + str(self.rgroupmenup),
+                                        fg='#db4455', font=fontm, bg='white')
+        # 메뉴 위치 지정
+        for i in range(0, self.rpersonnum):
+            self.rpersonmenu[i].place(x=275, y=160 + 40 * (i + 1))
+
+        # 개인 토탈금액 계산
+        for i in range(0, self.rpersonnum):
+            self.per_tolsum[i] += self.per_sum[i]
+            self.per_tolsum[i] += self.rgroupmenup
+            self.rpertotal += self.per_tolsum[i]
+
+        self.rper_tolsum[1 - 1] = Label(self.receipt, text=self.per_tolsum[1 - 1], fg='#db4455', font=fontm,
+                                        bg='white')
+        self.rper_tolsum[2 - 1] = Label(self.receipt, text=self.per_tolsum[2 - 1], fg='#db4455', font=fontm,
+                                        bg='white')
+        self.rper_tolsum[3 - 1] = Label(self.receipt, text=self.per_tolsum[3 - 1], fg='#db4455', font=fontm,
+                                        bg='white')
+        self.rper_tolsum[4 - 1] = Label(self.receipt, text=self.per_tolsum[4 - 1], fg='#db4455', font=fontm,
+                                        bg='white')
+        self.rper_tolsum[5 - 1] = Label(self.receipt, text=self.per_tolsum[5 - 1], fg='#db4455', font=fontm,
+                                        bg='white')
+        self.rper_tolsum[6 - 1] = Label(self.receipt, text=self.per_tolsum[6 - 1], fg='#db4455', font=fontm,
+                                        bg='white')
+
+        # 개인 총합 위치지정
+        for i in range(0, self.rpersonnum):
+            self.rper_tolsum[i].place(x=750, y=160 + 40 * (i + 1))
+
+        # 기존 총합 계산
+        for i in range(0, self.rpersonnum):
+            self.ori_price += self.per_sum[i]
+        self.ori_price += self.g_trans
+
+        # 반올림한 총합
+        for i in range(0, self.rpersonnum):
+            self.round_price += self.per_sum[i]
+            self.round_price += self.rgroupmenup
+
+        # 차액 계산 원래금액 - 전체 개인토탈금액 >0
+        self.odprice = self.ori_price - self.rpertotal
+
+        if self.odprice > 0:
+            self.over_price = self.odprice
+            self.under_price = 0
+        elif self.odprice < 0:
+            self.under_price = abs(self.odprice)
+            self.over_price = 0
+        else:
+            self.under_price = 0
+            self.over_price = 0
+
+        # 기존 금액 / 남은 금액 / 모자란 금액 440
+        self.price = Label(self.receipt, text="반올림 전 총액: " + str(self.ori_price) + "   반올림 후 총액: " + str(
+            self.round_price) + "   모자란 금액 : " + str(self.over_price) + "   남은 금액 :" + str(self.under_price),
+                           fg='#db4455', font=fonts, bg='white')
+        self.price.place(x=50, y=450)
+
+    def back(self):
+        #뒤로 갈 시에 저장된 최근 데이터 삭제
+        sqlCon = pymysql.connect(host="127.0.0.1", user="root", password="goodday0722", database="dutchdb",
+                                 charset="utf8")
+        cur = sqlCon.cursor()
+        cur.execute("delete from dutchdb where Id=%s", self.recentId)
+
+        sqlCon.commit()
+        sqlCon.close()
+        Move = menu_insert.Menuinsert(self.receipt, self.rpersonnum)
+
+    # menu_insert으로 넘어가기
+    def moveToStart(self):
+        Move = start.Start(self.receipt)
+
